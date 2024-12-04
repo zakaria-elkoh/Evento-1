5b7135e42121c63729958dbfa089893a1d7bad8a
zakaria-elkoh <zakaria.elkoh10@gmail.com>
2024-12-04 11:26:47 +0100

    testing the ci/cd

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="text-center pt-24">
       <h2 className="text-2xl">
-        Welcome to the Home page!!!!!
+        Welcome to the Home page after deployment!!!!!
       </h2>
     </div>
   );
